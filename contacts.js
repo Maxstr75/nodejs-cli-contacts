74f1266d11327a9715ea3b9755bf723037083357
Maxim Stribylo <max057521@gmail.com>
2022-09-04 00:45:16 +0300

    update

--- contacts.js
+++ contacts.js
@@ -1,23 +1,34 @@
 const fs = require("fs").promises;
+const path = require("path");
 
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const contactsPath = path.join(_dirname, "db", "contacts.json");
 
 // TODO: задокументировать каждую функцию
+
+// Получаем и выводим полный список контактов в таблицу
 function listContacts() {
   // ...твой код
 }
 
+// Получаем контакт по ИД
 function getContactById(contactId) {
   // ...твой код
 }
 
+// Удаляем контакт
 function removeContact(contactId) {
   // ...твой код
 }
 
+// Добавляем контакт
 function addContact(name, email, phone) {
   // ...твой код
 }
+
+//Экспортируем созданные функции
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
